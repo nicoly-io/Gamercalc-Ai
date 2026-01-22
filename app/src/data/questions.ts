8be663f58d05354f9e5929d97cb449812c8ea476
tanjiro <toumanicoly53@gmail.com>
2026-01-22 14:01:57 +0000

    Create questions.ts

---
+++ app/src/data/questions.ts
@@ -0,0 +1,9 @@
+export const quizData: any = {
+  "Mathématiques": {
+    "Débutant": [
+      { q: "Calculer 25 + 35", a: ["50", "60", "70"], c: 1 },
+      { q: "La racine de 144", a: ["11", "12", "13"], c: 1 }
+    ],
+    // ... AJOUTE TES 300 QUESTIONS ICI
+  }
+};
