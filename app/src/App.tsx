e0daede218aac963a29da1572b9e7cc8c9a9bdd3
tanjiro <toumanicoly53@gmail.com>
2026-01-22 20:15:08 +0000

    Create App.tsx

---
+++ app/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Gamert Calc AI</h1>
+      <p>L'application est en cours de chargement...</p>
+      <button onClick={() => alert('Calculatrice prête !')}>Tester</button>
+    </div>
+  );
+}
+
+export default App;
