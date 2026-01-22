afe937ea9b7e09fcc0a33d27f6f1f1c5d55c40ad
tanjiro <toumanicoly53@gmail.com>
2026-01-22 14:04:03 +0000

    Update conversions.ts

--- app/src/data/conversions.ts
+++ app/src/data/conversions.ts
@@ -1,1 +1,23 @@
-
+export const conversionRates: any = {
+  length: {
+    "Mètre (m)": 1,
+    "Kilomètre (km)": 0.001,
+    "Centimètre (cm)": 100,
+    "Millimètre (mm)": 1000,
+    "Mile": 0.000621371,
+    "Pouce (in)": 39.3701
+  },
+  weight: {
+    "Kilogramme (kg)": 1,
+    "Gramme (g)": 1000,
+    "Tonne (t)": 0.001,
+    "Livre (lb)": 2.20462,
+    "Once (oz)": 35.274
+  },
+  volume: {
+    "Litre (L)": 1,
+    "Millilitre (ml)": 1000,
+    "Mètre cube (m³)": 0.001,
+    "Gallon (US)": 0.264172
+  }
+};
